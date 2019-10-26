ac337b45b47092b072c4dd19c0e0600977ed2f42
Anthony <anthony@hevia.dev>
2019-10-26 13:50:24 -0400

    modifying boilerplate code and gitignore

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,27 +1,8 @@
 import React, { useState, useEffect } from "react";
-import pet, { ANIMALS } from "@frontendmasters/pet";
-import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
   // First item is the string, second item is the updater function
-  const [location, setLocation] = useState("Seattle, WA"); // This is a hook
-  const [breeds, setBreeds] = useState([]);
-  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
-
-  // Disconnected from the actual render
-  // Rendering happening first before this is called
-  // This will set the "breeds" variable
-  // This allows for better UX so they see something right away
-  useEffect(() => {
-    setBreeds([]);
-    setBreed("");
-    pet.breeds(animal).then(({ breeds }) => {
-      const breedStrings = breeds.map(({ name }) => name);
-      setBreeds(breedStrings);
-    }, console.error);
-  }, [animal, setBreed, setBreeds]);
-  // This array at the end is a list of dependencies for this useEffect so it only gets called when one of these is changed
+  const [location, setLocation] = useState("Atlanta, GA"); // This is a hook
 
   return (
     <div className="search-params">
@@ -35,8 +16,6 @@ const SearchParams = () => {
             onChange={event => setLocation(event.target.value)} // weeny tiney function
           />
         </label>
-        <AnimalDropdown />
-        <BreedDropdown />
         <button>Submit</button>
       </form>
     </div>
